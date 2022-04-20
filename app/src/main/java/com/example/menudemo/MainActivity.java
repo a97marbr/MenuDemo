7188848264ecbf96d8613e061208c77d2dabfb0f
a97marbr <a97marbr@gmail.com>
2022-04-20 13:55:06 +0200

    Inflated the menu in main activity.

--- app/src/main/java/com/example/menudemo/MainActivity.java
+++ app/src/main/java/com/example/menudemo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.menudemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 }
